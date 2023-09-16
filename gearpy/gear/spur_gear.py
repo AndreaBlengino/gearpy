743130eb70af0c029cd9c8fe05bf52826718018b
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-09-16 23:00:28 +0200

    DEV: create SpurGear

---
+++ gearpy/gear/spur_gear.py
@@ -0,0 +1,63 @@
+from .gear import GearBase
+
+
+class SpurGear(GearBase):
+
+    def __init__(self, name, n_teeth):
+        super().__init__(name = name, n_teeth = n_teeth)
+
+    @property
+    def name(self):
+        return super().name
+
+    @property
+    def n_teeth(self):
+        return super().n_teeth
+
+    @property
+    def driven_by(self):
+        return super().driven_by
+
+    @driven_by.setter
+    def driven_by(self, driven_by):
+        super(SpurGear, type(self)).driven_by.fset(self, driven_by)
+
+    @property
+    def drives(self):
+        return super().drives
+
+    @drives.setter
+    def drives(self, drives):
+        super(SpurGear, type(self)).drives.fset(self, drives)
+
+    @property
+    def master_gear_ratio(self):
+        return super().master_gear_ratio
+
+    @master_gear_ratio.setter
+    def master_gear_ratio(self, master_gear_ratio):
+        super(SpurGear, type(self)).master_gear_ratio.fset(self, master_gear_ratio)
+
+    @property
+    def angle(self):
+        return super().angle
+
+    @angle.setter
+    def angle(self, angle):
+        super(SpurGear, type(self)).angle.fset(self, angle)
+
+    @property
+    def speed(self):
+        return super().speed
+
+    @speed.setter
+    def speed(self, speed):
+        super(SpurGear, type(self)).speed.fset(self, speed)
+
+    @property
+    def acceleration(self):
+        return super().acceleration
+
+    @acceleration.setter
+    def acceleration(self, acceleration):
+        super(SpurGear, type(self)).acceleration.fset(self, acceleration)
