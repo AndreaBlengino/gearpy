dd6414a5309d3c701825e59d804470f78471f94d
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-09-16 23:04:46 +0200

    DEV: create DCMotor

---
+++ gearpy/motor/dc_motor.py
@@ -0,0 +1,53 @@
+from .motor import MotorBase
+
+
+class DCMotor(MotorBase):
+
+    def __init__(self, name, no_load_speed, maximum_torque):
+        super().__init__(name = name)
+        self.__no_load_speed = no_load_speed
+        self.__maximum_torque = maximum_torque
+
+    @property
+    def name(self):
+        return super().name
+
+    @property
+    def drives(self):
+        return super().drives
+
+    @drives.setter
+    def drives(self, drives):
+        super(DCMotor, type(self)).drives.fset(self, drives)
+
+    @property
+    def angle(self):
+        return super().angle
+
+    @angle.setter
+    def angle(self, angle):
+        super(DCMotor, type(self)).angle.fset(self, angle)
+
+    @property
+    def speed(self):
+        return super().speed
+
+    @speed.setter
+    def speed(self, speed):
+        super(DCMotor, type(self)).speed.fset(self, speed)
+
+    @property
+    def acceleration(self):
+        return super().acceleration
+
+    @acceleration.setter
+    def acceleration(self, acceleration):
+        super(DCMotor, type(self)).acceleration.fset(self, acceleration)
+
+    @property
+    def no_load_speed(self):
+        return self.__no_load_speed
+
+    @property
+    def maximum_torque(self):
+        return self.__maximum_torque
