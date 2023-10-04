e173aabd80c5d631a35b9dc637ca01cd8edc1056
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-10-05 00:35:49 +0200

    DEV: add input check on MechanicalObject

--- gearpy/mechanical_object/mechanical_object.py
+++ gearpy/mechanical_object/mechanical_object.py
@@ -4,7 +4,13 @@ from abc import ABC, abstractmethod
 class MechanicalObject(ABC):
 
     @abstractmethod
-    def __init__(self, name):
+    def __init__(self, name: str):
+        if not isinstance(name, str):
+            raise TypeError("Parameter 'name' must be a string.")
+
+        if name == '':
+            raise ValueError("Parameter 'name' cannot be an empty string")
+
         self.__name = name
 
     @property
