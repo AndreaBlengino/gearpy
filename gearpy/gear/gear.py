84c4aba9b777825d2b68ae0297a5065f3bb73399
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-10-05 00:58:38 +0200

    DEV: add input check on gears

--- gearpy/gear/gear.py
+++ gearpy/gear/gear.py
@@ -1,12 +1,20 @@
 from abc import abstractmethod
 from gearpy.mechanical_object.rotating_object import RotatingObject
+from typing import Callable
 
 
 class GearBase(RotatingObject):
 
     @abstractmethod
-    def __init__(self, name, n_teeth, inertia):
+    def __init__(self, name: str, n_teeth: int, inertia: float):
         super().__init__(name = name, inertia = inertia)
+
+        if not isinstance(n_teeth, int):
+            raise TypeError("Parameter 'n_teeth' must be an integer.")
+
+        if n_teeth <= 0:
+            raise ValueError("Parameter 'n_teeth' must be positive.")
+
         self.__n_teeth = n_teeth
         self.__driven_by = None
         self.__drives = None
@@ -16,55 +24,67 @@ class GearBase(RotatingObject):
 
     @property
     @abstractmethod
-    def n_teeth(self):
+    def n_teeth(self) -> int:
         return self.__n_teeth
 
     @property
     @abstractmethod
-    def driven_by(self):
+    def driven_by(self) -> RotatingObject:
         return self.__driven_by
 
     @driven_by.setter
     @abstractmethod
-    def driven_by(self, driven_by):
+    def driven_by(self, driven_by: RotatingObject):
         self.__driven_by = driven_by
 
     @property
     @abstractmethod
-    def drives(self):
+    def drives(self) -> RotatingObject:
         return self.__drives
 
     @drives.setter
     @abstractmethod
-    def drives(self, drives):
+    def drives(self, drives: RotatingObject):
         self.__drives = drives
 
     @property
     @abstractmethod
-    def master_gear_ratio(self):
+    def master_gear_ratio(self) -> float:
         return self.__master_gear_ratio
 
     @master_gear_ratio.setter
     @abstractmethod
-    def master_gear_ratio(self, master_gear_ratio):
+    def master_gear_ratio(self, master_gear_ratio: float):
+        if not isinstance(master_gear_ratio, float):
+            raise TypeError("Parameter 'master_gear_ratio' must be a float.")
+
         self.__master_gear_ratio = master_gear_ratio
 
     @property
     @abstractmethod
-    def master_gear_efficiency(self):
+    def master_gear_efficiency(self) -> float:
         return self.__master_gear_efficiency
 
     @master_gear_efficiency.setter
     @abstractmethod
-    def master_gear_efficiency(self, master_gear_efficiency):
+    def master_gear_efficiency(self, master_gear_efficiency: float):
+        if not isinstance(master_gear_efficiency, float) and not isinstance(master_gear_efficiency, int):
+            raise TypeError("Parameter 'master_gear_efficiency' must be a float or an integer.")
+
+        if master_gear_efficiency > 1 or master_gear_efficiency < 0:
+            raise ValueError("Parameter 'master_gear_efficiency' must be within 0 and 1.")
+
         self.__master_gear_efficiency = master_gear_efficiency
 
     @property
     @abstractmethod
-    def external_torque(self):
+    def external_torque(self) -> Callable:
         return self.__external_torque
 
     @external_torque.setter
     @abstractmethod
-    def external_torque(self, external_torque):
+    def external_torque(self, external_torque: Callable):
+        if not isinstance(external_torque, Callable):
+            raise TypeError("Parameter 'external_torque' must be callable.")
+
         self.__external_torque = external_torque
--- gearpy/gear/spur_gear.py
+++ gearpy/gear/spur_gear.py
@@ -1,113 +1,115 @@
 from .gear import GearBase
+from gearpy.mechanical_object.rotating_object import RotatingObject
+from typing import Callable
 
 
 class SpurGear(GearBase):
 
-    def __init__(self, name, n_teeth, inertia):
+    def __init__(self, name: str, n_teeth: int, inertia: float):
         super().__init__(name = name, n_teeth = n_teeth, inertia = inertia)
 
     @property
-    def name(self):
+    def name(self) -> str:
         return super().name
 
     @property
-    def n_teeth(self):
+    def n_teeth(self) -> int:
         return super().n_teeth
 
     @property
-    def driven_by(self):
+    def driven_by(self) -> RotatingObject:
         return super().driven_by
 
     @driven_by.setter
-    def driven_by(self, driven_by):
+    def driven_by(self, driven_by: RotatingObject):
         super(SpurGear, type(self)).driven_by.fset(self, driven_by)
 
     @property
-    def drives(self):
+    def drives(self) -> RotatingObject:
         return super().drives
 
     @drives.setter
-    def drives(self, drives):
+    def drives(self, drives: RotatingObject):
         super(SpurGear, type(self)).drives.fset(self, drives)
 
     @property
-    def master_gear_ratio(self):
+    def master_gear_ratio(self) -> float:
         return super().master_gear_ratio
 
     @master_gear_ratio.setter
-    def master_gear_ratio(self, master_gear_ratio):
+    def master_gear_ratio(self, master_gear_ratio: float):
         super(SpurGear, type(self)).master_gear_ratio.fset(self, master_gear_ratio)
 
     @property
-    def master_gear_efficiency(self):
+    def master_gear_efficiency(self) -> float:
         return super().master_gear_efficiency
 
     @master_gear_efficiency.setter
-    def master_gear_efficiency(self, master_gear_efficiency):
+    def master_gear_efficiency(self, master_gear_efficiency: float):
         super(SpurGear, type(self)).master_gear_efficiency.fset(self, master_gear_efficiency)
 
     @property
-    def angle(self):
+    def angle(self) -> float:
         return super().angle
 
     @angle.setter
-    def angle(self, angle):
+    def angle(self, angle: float):
         super(SpurGear, type(self)).angle.fset(self, angle)
 
     @property
-    def speed(self):
+    def speed(self) -> float:
         return super().speed
 
     @speed.setter
-    def speed(self, speed):
+    def speed(self, speed: float):
         super(SpurGear, type(self)).speed.fset(self, speed)
 
     @property
-    def acceleration(self):
+    def acceleration(self) -> float:
         return super().acceleration
 
     @acceleration.setter
-    def acceleration(self, acceleration):
+    def acceleration(self, acceleration: float):
         super(SpurGear, type(self)).acceleration.fset(self, acceleration)
 
     @property
-    def torque(self):
+    def torque(self) -> float:
         return super().torque
 
     @torque.setter
-    def torque(self, torque):
+    def torque(self, torque: float):
         super(SpurGear, type(self)).torque.fset(self, torque)
 
     @property
-    def driving_torque(self):
+    def driving_torque(self) -> float:
         return super().driving_torque
 
     @driving_torque.setter
-    def driving_torque(self, driving_torque):
+    def driving_torque(self, driving_torque: float):
         super(SpurGear, type(self)).driving_torque.fset(self, driving_torque)
 
     @property
-    def load_torque(self):
+    def load_torque(self) -> float:
         return super().load_torque
 
     @load_torque.setter
-    def load_torque(self, load_torque):
+    def load_torque(self, load_torque: float):
         super(SpurGear, type(self)).load_torque.fset(self, load_torque)
 
     @property
-    def inertia(self):
+    def inertia(self) -> float:
         return super().inertia
 
     @property
-    def external_torque(self):
+    def external_torque(self) -> Callable:
         return super().external_torque
 
     @external_torque.setter
-    def external_torque(self, external_torque):
+    def external_torque(self, external_torque: Callable):
         super(SpurGear, type(self)).external_torque.fset(self, external_torque)
 
     @property
-    def time_variables(self):
+    def time_variables(self) -> dict:
         return super().time_variables
 
     def update_time_variables(self):
