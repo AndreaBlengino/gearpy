e93aab3ded473ff59d9c8a00255c42845cd974d5
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-10-07 23:59:24 +0200

    TST: create Solver tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
-from gearpy import DCMotor, SpurGear
+from gearpy import DCMotor, SpurGear, Transmission, add_fixed_joint, add_gear_mating
 from gearpy.gear.gear import GearBase
 from gearpy.motor.motor import MotorBase
 from gearpy.mechanical_object.rotating_object import RotatingObject
-from hypothesis.strategies import composite, text, integers, floats
+from hypothesis.strategies import composite, text, integers, floats, lists
 import numpy as np
 from pytest import fixture
 from typing import Callable
@@ -19,9 +19,9 @@ basic_rotating_objects = [basic_dc_motor, basic_spur_gear]
 @composite
 def dc_motors(draw):
     name = draw(text(min_size = 1))
-    inertia = draw(floats(min_value = 0, exclude_min = True))
-    no_load_speed = draw(floats(min_value = 0, exclude_min = True))
-    maximum_torque = draw(floats(min_value = 0, exclude_min = True))
+    inertia = draw(floats(min_value = 0, exclude_min = True, allow_nan = False, allow_infinity = False))
+    no_load_speed = draw(floats(min_value = 0, exclude_min = True, allow_nan = False, allow_infinity = False))
+    maximum_torque = draw(floats(min_value = 0, exclude_min = True, allow_nan = False, allow_infinity = False))
 
     return DCMotor(name = name, inertia = inertia, no_load_speed = no_load_speed, maximum_torque = maximum_torque)
 
@@ -30,9 +30,25 @@ def dc_motors(draw):
 def spur_gears(draw):
     name = draw(text(min_size = 1))
     n_teeth = draw(integers(min_value = 1))
-    inertia = draw(floats(min_value = 0, exclude_min = True))
+    inertia = draw(floats(min_value = 0, exclude_min = True, allow_nan = False, allow_infinity = False))
 
     return SpurGear(name = name, n_teeth = n_teeth, inertia = inertia)
+
+
+@composite
+def transmissions(draw):
+    motor = draw(dc_motors())
+    gears = draw(lists(elements = spur_gears(), min_size = 1))
+
+    add_fixed_joint(master = motor, slave = gears[0])
+
+    for i in range(0, len(gears) - 1):
+        if i%2 == 0:
+            add_gear_mating(master = gears[i], slave = gears[i + 1], efficiency = 1)
+        else:
+            add_fixed_joint(master = gears[i], slave = gears[i + 1])
+
+    return Transmission(motor = motor)
 
 
 @fixture(params = [type_to_check for type_to_check in types_to_check if not isinstance(type_to_check, float)
@@ -187,4 +203,76 @@ def add_fixed_joint_type_error(request):
 
 @fixture(params = [type_to_check for type_to_check in types_to_check if not isinstance(type_to_check, MotorBase)])
 def transmission_init_type_error(request):
+    return request.param
+
+
+motor_transmission_solver_init_type_error = DCMotor(name = 'name', inertia = 1, no_load_speed = 1, maximum_torque = 1)
+gear_transmission_solver_init_type_error = SpurGear(name = 'gear', n_teeth = 10, inertia = 1)
+add_fixed_joint(master = motor_transmission_solver_init_type_error, slave = gear_transmission_solver_init_type_error)
+transmission_solver_init_type_error = Transmission(motor = motor_transmission_solver_init_type_error)
+
+solver_init_type_error_1 = [{'time_discretization': type_to_check, 'simulation_time': 1,
+                             'transmission': transmission_solver_init_type_error} for type_to_check in types_to_check
+                            if not isinstance(type_to_check, float) and not isinstance(type_to_check, int)
+                            and not isinstance(type_to_check, bool)]
+
+solver_init_type_error_2 = [{'time_discretization': 1, 'simulation_time': type_to_check,
+                             'transmission': transmission_solver_init_type_error} for type_to_check in types_to_check
+                            if not isinstance(type_to_check, float) and not isinstance(type_to_check, int)
+                            and not isinstance(type_to_check, bool)]
+
+solver_init_type_error_3 = [{'time_discretization': 1, 'simulation_time': 5,
+                             'transmission': type_to_check} for type_to_check in types_to_check
+                            if not isinstance(type_to_check, Transmission)]
+
+motor_gear_pairs_4 = {DCMotor(name = 'motor', inertia = 1, no_load_speed = 1, maximum_torque = 1):
+                      SpurGear(name = 'motor', n_teeth = 10, inertia = 1)
+                      for type_to_check in types_to_check if not isinstance(type_to_check, MotorBase)}
+for motor, gear in motor_gear_pairs_4.items():
+    add_fixed_joint(master = motor, slave = gear)
+transmissions_4 = [Transmission(motor) for motor in motor_gear_pairs_4.keys()]
+for transmission_4, type_to_check in zip(transmissions_4, types_to_check):
+    if not isinstance(type_to_check, MotorBase):
+        transmission_4.chain[0] = type_to_check
+solver_init_type_error_4 = [{'time_discretization': 1, 'simulation_time': 5, 'transmission': transmission_4}
+                            for transmission_4 in transmissions_4]
+
+motor_gear_pairs_5 = {DCMotor(name = 'motor', inertia = 1, no_load_speed = 1, maximum_torque = 1):
+                      SpurGear(name = 'motor', n_teeth = 10, inertia = 1)
+                      for type_to_check in types_to_check if not isinstance(type_to_check, RotatingObject)}
+for motor, gear in motor_gear_pairs_5.items():
+    add_fixed_joint(master = motor, slave = gear)
+transmissions_5 = [Transmission(motor) for motor in motor_gear_pairs_5.keys()]
+for transmission_5, type_to_check in zip(transmissions_5, types_to_check):
+    if not isinstance(type_to_check, RotatingObject):
+        transmission_5.chain[1] = type_to_check
+solver_init_type_error_5 = [{'time_discretization': 1, 'simulation_time': 5, 'transmission': transmission_5}
+                            for transmission_5 in transmissions_5]
+
+@fixture(params = [*solver_init_type_error_1,
+                   *solver_init_type_error_2,
+                   *solver_init_type_error_3,
+                   *solver_init_type_error_4,
+                   *solver_init_type_error_5])
+def solver_init_type_error(request):
+    return request.param
+
+motor_transmission_solver_init_value_error_1 = DCMotor(name = 'name', inertia = 1,
+                                                       no_load_speed = 1, maximum_torque = 1)
+gear_transmission_solver_init_value_error_1 = SpurGear(name = 'gear', n_teeth = 10, inertia = 1)
+add_fixed_joint(master = motor_transmission_solver_init_value_error_1, slave = gear_transmission_solver_init_value_error_1)
+transmission_solver_init_value_error_1 = Transmission(motor = motor_transmission_solver_init_value_error_1)
+
+motor_transmission_solver_init_value_error_2 = DCMotor(name = 'name', inertia = 1,
+                                                       no_load_speed = 1, maximum_torque = 1)
+gear_transmission_solver_init_value_error_2 = SpurGear(name = 'gear', n_teeth = 10, inertia = 1)
+add_fixed_joint(master = motor_transmission_solver_init_value_error_2, slave = gear_transmission_solver_init_value_error_2)
+transmission_solver_init_value_error_2 = Transmission(motor = motor_transmission_solver_init_value_error_2)
+transmission_solver_init_value_error_2.chain.clear()
+
+@fixture(params = [{'time_discretization': -1, 'simulation_time': 5, 'transmission': transmission_solver_init_value_error_1},
+                   {'time_discretization': 1, 'simulation_time': -1, 'transmission': transmission_solver_init_value_error_1},
+                   {'time_discretization': 6, 'simulation_time': 5, 'transmission': transmission_solver_init_value_error_1},
+                   {'time_discretization': 1, 'simulation_time': 5, 'transmission': transmission_solver_init_value_error_2}])
+def solver_init_value_error(request):
     return request.param
---
+++ tests/test_solver.py
@@ -0,0 +1,68 @@
+from gearpy import Solver
+from hypothesis import given, settings
+from hypothesis.strategies import floats, integers
+import numpy as np
+from pytest import mark, raises
+from tests.conftest import transmissions
+
+
+@mark.solver
+class TestSolverInit:
+
+
+    @mark.genuine
+    @given(time_discretization = floats(min_value = 0, max_value = 1, exclude_min = True,
+                                        allow_nan = False, allow_infinity = False),
+           simulation_steps = integers(min_value = 2, max_value = 1000),
+           transmission = transmissions())
+    @settings(max_examples = 100)
+    def test_method(self, time_discretization, simulation_steps, transmission):
+        simulation_time = time_discretization*simulation_steps
+        solver = Solver(time_discretization = time_discretization,
+                        simulation_time = simulation_time,
+                        transmission = transmission)
+
+        assert solver.time_discretization == time_discretization
+        assert solver.simulation_time == simulation_time
+        assert solver.transmission_chain == transmission.chain
+        assert solver.time == [0]
+
+
+    @mark.error
+    def test_raises_type_error(self, solver_init_type_error):
+        with raises(TypeError):
+            Solver(time_discretization = solver_init_type_error['time_discretization'],
+                   simulation_time = solver_init_type_error['simulation_time'],
+                   transmission = solver_init_type_error['transmission'])
+
+
+    @mark.error
+    def test_raises_value_error(self, solver_init_value_error):
+        with raises(ValueError):
+            Solver(time_discretization = solver_init_value_error['time_discretization'],
+                   simulation_time = solver_init_value_error['simulation_time'],
+                   transmission = solver_init_value_error['transmission'])
+
+
+@mark.solver
+class TestSolverRun:
+
+
+    @mark.genuine
+    @given(time_discretization = floats(min_value = 1e-10, max_value = 10, allow_nan = False, allow_infinity = False),
+           simulation_steps = floats(min_value = 5, max_value = 1000, allow_nan = False, allow_infinity = False),
+           transmission = transmissions(),
+           initial_angle = floats(allow_nan = False, allow_infinity = False),
+           initial_speed = floats(allow_nan = False, allow_infinity = False))
+    @settings(max_examples = 100, deadline = None)
+    def test_method(self, time_discretization, simulation_steps, transmission, initial_angle, initial_speed):
+        transmission.chain[-1].angle = initial_angle
+        transmission.chain[-1].speed = initial_speed
+        transmission.chain[-1].external_torque = lambda time, angle, speed: 0.001
+        simulation_time = time_discretization*simulation_steps
+        solver = Solver(time_discretization = time_discretization,
+                        simulation_time = simulation_time,
+                        transmission = transmission)
+        solver.run()
+
+        assert len(solver.time) == len(np.arange(time_discretization, simulation_time, time_discretization)) + 1
