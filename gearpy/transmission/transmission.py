2f4425201a72ab5f9391da1aea6028845e01b11a
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-10-07 17:48:32 +0200

    DEV: add input check on Transmission

--- gearpy/transmission/transmission.py
+++ gearpy/transmission/transmission.py
@@ -4,6 +4,12 @@ from gearpy.motor.motor import MotorBase
 class Transmission:
 
     def __init__(self, motor: MotorBase):
+        if not isinstance(motor, MotorBase):
+            raise TypeError("Parameter 'motor' must be an instance of MotorBase.")
+
+        if motor.drives is None:
+            raise ValueError("Parameter 'motor' is not connected to any other element. Call 'add_fixed_joint' "
+                             "to join 'motor' with a GearBase's instance.")
 
         self.chain = [motor]
         while self.chain[-1].drives is not None:
