e8d7a3d3e23080abb4b1a7696c1591e1299e0736
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-10-09 23:17:35 +0200

    DEV: add RotatingObject abstract properties to MotorBase and GearBase

--- gearpy/motor/motor.py
+++ gearpy/motor/motor.py
@@ -23,5 +23,65 @@ class MotorBase(RotatingObject):
 
         self.__drives = drives
 
+    @property
+    @abstractmethod
+    def angle(self) -> Union[float, int]:
+        return super().angle
+
+    @angle.setter
+    @abstractmethod
+    def angle(self, angle: Union[float, int]):
+        super(MotorBase, type(self)).angle.fset(self, angle)
+
+    @property
+    @abstractmethod
+    def speed(self) -> Union[float, int]:
+        return super().speed
+
+    @speed.setter
+    @abstractmethod
+    def speed(self, speed: Union[float, int]):
+        super(MotorBase, type(self)).speed.fset(self, speed)
+
+    @property
+    @abstractmethod
+    def acceleration(self) -> Union[float, int]:
+        return super().acceleration
+
+    @acceleration.setter
+    @abstractmethod
+    def acceleration(self, acceleration: Union[float, int]):
+        super(MotorBase, type(self)).acceleration.fset(self, acceleration)
+
+    @property
+    @abstractmethod
+    def torque(self) -> Union[float, int]:
+        return super().torque
+
+    @torque.setter
+    @abstractmethod
+    def torque(self, torque: Union[float, int]):
+        super(MotorBase, type(self)).torque.fset(self, torque)
+
+    @property
+    @abstractmethod
+    def driving_torque(self) -> Union[float, int]:
+        return super().driving_torque
+
+    @driving_torque.setter
+    @abstractmethod
+    def driving_torque(self, driving_torque: Union[float, int]):
+        super(MotorBase, type(self)).driving_torque.fset(self, driving_torque)
+
+    @property
+    @abstractmethod
+    def load_torque(self) -> Union[float, int]:
+        return super().load_torque
+
+    @load_torque.setter
+    @abstractmethod
+    def load_torque(self, load_torque: Union[float, int]):
+        super(MotorBase, type(self)).load_torque.fset(self, load_torque)
+
     @abstractmethod
     def compute_torque(self): ...
