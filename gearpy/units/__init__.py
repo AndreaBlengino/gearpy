d769c8da15960f831ffdedd7fb275586dbe24e02
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-10-14 01:28:33 +0200

    DEV: move concrete unit classes in a single file
    In order to avoid circular import, all concrete unit classes are moved
    to a single file

--- gearpy/units/__init__.py
+++ gearpy/units/__init__.py
@@ -1,8 +1,7 @@
-from .angular_position import AngularPosition
-from .angular_speed import AngularSpeed
-from .angular_acceleration import AngularAcceleration
-from .inertia_moment import InertiaMoment
-from .torque import Torque
-from .time import Time
-from .time_interval import TimeInterval
-
+from .concrete_units import AngularPosition, \
+                            AngularSpeed, \
+                            AngularAcceleration, \
+                            InertiaMoment, \
+                            Torque, \
+                            Time, \
+                            TimeInterval
