84fc8cc406e1a9bfa3a3697e9b7244def8f3fa4b
Andrea Blengino <ing.andrea.blengino@gmail.com>
2024-01-14 23:42:45 +0100

    DOC: add rules documentation docstrings

--- gearpy/motor_control/rules.py
+++ gearpy/motor_control/rules.py
@@ -8,6 +8,27 @@ from typing import Union, Optional
 
 
 class ReachAngularPosition(RuleBase):
+    """gearpy.motor_control.rules.ReachAngularPosition object. \n
+    It can be used to make the ``encoder``'s ``target`` reach a ``target_angular_position`` within a ``braking_angle``.
+
+    Methods
+    -------
+    :py:meth:`apply`
+        Computes the ``pwm`` to apply to the ``transmission`` motor in order to reach a ``target_angular_position`` by
+        the ``target`` rotating object of the ``encoder``, within a specific ``braking_angle``.
+
+    Raises
+    ------
+    TypeError
+        - If ``encoder`` is not an instance of ``AbsoluteRotaryEncoder``,
+        - if ``transmission`` is not an instance of ``Transmission``,
+        - if the first element in ``transmission`` is not an instance of ``MotorBase``,
+        - if an element of ``transmission`` is not an instance of ``RotatingObject``,
+        - if ``target_angular_position`` is not an instance of ``AngularPosition``,
+        - if ``braking_angle`` is not an instance of ``Angle``.
+    ValueError
+        If ``transmission.chain`` is an empty tuple.
+    """
 
     def __init__(self,
                  encoder: AbsoluteRotaryEncoder,
@@ -42,7 +63,57 @@ class ReachAngularPosition(RuleBase):
         self.target_angular_position = target_angular_position
         self.braking_angle = braking_angle
 
-    def apply(self) -> Union[float, int]:
+    def apply(self) -> Union[None, float, int]:
+        r"""Computes the ``pwm`` to apply to the ``transmission`` motor in order to reach a ``target_angular_position``
+        by the ``target`` rotating object of the ``encoder``, within a specific ``braking_angle``.
+
+        Returns
+        -------
+        float or int or None
+            PWM value to apply to the motor in order to reach the target angular position.
+
+        Notes
+        -----
+        It computes the ``transmission``'s *static error* according to the following formula:
+
+        .. math::
+            \theta_{err} \left( T_l \right) = \frac{T_l}{T_{max}} \, \frac{\theta_b}{\eta_t}
+
+        where:
+
+        - :math:`\theta_{err}` is the ``transmission`` static error,
+        - :math:`T_l` is the load torque on the ``transmission`` DC motor,
+        - :math:`T_{max}` is the maximum torque developed by the ``transmission`` DC motor,
+        - :math:`\theta_b` is the ``braking_angle`` parameter,
+        - :math:`\eta_t` is the ``transmission`` overall efficiency, computed as:
+
+        .. math::
+            \eta_t = \prod_{i = 1}^N \eta_i
+
+        where:
+
+        - :math:`\eta_i` is the mechanical mating efficiency of the mating between two gears,
+        - :math:`N` is the total number of gear matings in the ``transmission``.
+
+        Then it checks the applicability condition, defined as:
+
+        .. math::
+            \theta \ge \theta_s = \theta_t - \theta_b + \theta_{err}
+
+        where:
+
+        - :math:`\theta` is the ``encoder``'s ``target`` ``angular_position``,
+        - :math:`\theta_s` is the braking starting angle,
+        - :math:`\theta_t` is the ``target_angular_position`` parameter.
+
+        If the applicability condition is not met, then it returns ``None``, otherwise it computes the ``pwm`` as:
+
+        .. math::
+            D \left( \theta \right) = 1 - \frac{\theta - \theta_s}{\theta_b}
+
+        where :math:`D` is the supply voltage PWM duty cycle (``pwm``) to apply to the DC motor in order to reach the
+        ``target_angular_position`` by the ``encoder``'s ``target`` rotating object.
+        """
         angular_position = self.encoder.get_value()
 
         regime_angular_position_error = _compute_static_error(braking_angle = self.braking_angle,
@@ -54,6 +125,37 @@ class ReachAngularPosition(RuleBase):
 
 
 class StartProportionalToAngularPosition(RuleBase):
+    """gearpy.motor_control.rules.StartProportionalToAngularPosition object. \n
+    It can be used to make a gradual start of the ``transmission``'s motion, in order to avoid a peak in the
+    ``transmission``'s DC motor absorbed electric current. \n
+    It computes a ``pwm`` to apply to the ``transmission``'s DC motor which increases linearly with the ``encoder``'s
+    ``target`` rotating object's ``angular_position``. The computed ``pwm`` starts from a minimum value, based on
+    ``transmission``'s elements properties and ``pwm_min_multiplier`` or solely on ``pwm_min`` parameter. The computed
+    ``pwm`` increases up to ``1`` when the ``encoder``'s ``target``'s ``angular_position`` equals
+    ``target_angular_position``.
+
+    Methods
+    -------
+    :py:meth:`apply`
+        Computes the ``pwm`` to apply to the ``transmission`` motor, proportional to the ``encoder``'s ``target``
+        ``angular_position`` until it reaches the ``target_angular_position``.
+
+    Raises
+    ------
+    TypeError
+        - If ``encoder`` is not an instance of ``AbsoluteRotaryEncoder``,
+        - if ``transmission`` is not an instance of ``Transmission``,
+        - if the first element in ``transmission`` is not an instance of ``MotorBase``,
+        - if an element of ``transmission`` is not an instance of ``RotatingObject``,
+        - if ``target_angular_position`` is not an instance of ``AngularPosition``,
+        - if ``pwm_min_multiplier`` is not a float or an integer,
+        - if ``pwm_min`` is defined and it is not a float or an integer.
+    ValueError
+        - If ``transmission.chain`` is an empty tuple,
+        - if the ``transmission`` motor cannot compute ``electric_current`` property,
+        - if ``pwm_min_multiplier`` is less than or equal to ``1``,
+        - if ``pwm_min`` is defined and it is negative or null.
+    """
 
     def __init__(self,
                  encoder: AbsoluteRotaryEncoder,
@@ -103,7 +205,75 @@ class StartProportionalToAngularPosition(RuleBase):
         self.pwm_min_multiplier = pwm_min_multiplier
         self.pwm_min = pwm_min
 
-    def apply(self) -> Union[float, int]:
+    def apply(self) -> Union[None, float, int]:
+        r"""Computes the ``pwm`` to apply to the ``transmission`` motor, proportional to the ``encoder``'s ``target``
+        ``angular_position`` until it reaches the ``target_angular_position``.
+
+        Returns
+        -------
+        float or int or None
+            PWM value to apply to the motor, proportional to the ``encoder``'s ``target`` ``angular_position``.
+
+        Notes
+        -----
+        It computes a *candidate* minimum applicable ``pwm`` as:
+
+        .. math::
+            D_{min}^c \left( T_l \right) = \frac{1}{\eta_t} \, \frac{T_l}{T_{max}} \, \frac{i_{max} - i_0}{i_{max}} +
+            \frac{i_0}{i_{max}}
+
+        where:
+
+        - :math:`D_{min}^c` is the *candidate* minimum applicable ``pwm``,
+        - :math:`T_l` is the load torque on the ``transmission`` DC motor,
+        - :math:`T_{max}` is the maximum torque developed by the ``transmission`` DC motor,
+        - :math:`i_{max}` is the maximum electric current absorbed by the ``transmission`` DC motor,
+        - :math:`i_0` is the no load electric current absorbed by the ``transmission`` DC motor,
+        - :math:`\eta_t` is the ``transmission`` overall efficiency, computed as:
+
+        .. math::
+            \eta_t = \prod_{i = 1}^N \eta_i
+
+        where:
+
+        - :math:`\eta_i` is the mechanical mating efficiency of the mating between two gears,
+        - :math:`N` is the total number of gear matings in the ``transmission``.
+
+        If both the load torque on the ``transmission`` DC motor :math:`T_l` and the motor no load electric current
+        :math:`i_0` are null, then also the computed *candidate* minimum applicable ``pwm`` :math:`D_{min}^c` is null.
+        Only in this case, the compuded *candidate* minimum applicable ``pwm`` is discarded and it is taken into account
+        the ``pwm_min`` parameter, which must have been set (don't used otherwise):
+
+        .. math::
+            D_{min} = D_{min}^p
+
+        where:
+
+        - :math:`D_{min}` is the minimum applicable ``pwm``,
+        - :math:`D_{min}^p` is the ``pwm_min`` parameter.
+
+        Otherwise, the *candidate* :math:`D_{min}` is multiplied by the ``pwm_min_multiplier`` parameter:
+
+        .. math::
+            D_{min} = D_{min}^c \, g
+
+        where :math:`g` is the ``pwm_min_multiplier`` parameter.
+
+        Then it checks the applicability condition, defined as:
+
+        .. math::
+            \theta \le \theta_t
+
+        where:
+
+        - :math:`\theta` is the ``encoder``'s ``target`` ``angular_position``,
+        - :math:`\theta_t` is the ``target_angular_position``.
+
+        If the applicability condition is not met, then it returns ``None``, otherwise it computes the ``pwm`` as:
+
+        .. math::
+            D \left( \theta \right) = \left( 1 - D_{min} \right) \frac{\theta}{\theta_t} + D_{min}
+        """
         angular_position = self.encoder.get_value()
 
         computed_pwm_min = self.pwm_min_multiplier*_compute_pwm_min(transmission = self.transmission)
@@ -119,6 +289,31 @@ class StartProportionalToAngularPosition(RuleBase):
 
 
 class StartLimitCurrent(RuleBase):
+    """gearpy.motor_control.rules.StartLimitCurrent object. \n
+    It can be used to make a gradual start of the transmission's motion and limit the ``motor`` absorbed electric
+    current to be lower than a ``limit_electric_current`` value. It computes a ``pwm`` to apply to the ``motor`` up
+    until the ``encoder``'s ``target``'s ``angular_position`` equals ``target_angular_position``. \n
+    For an optimal ``pwm`` management, it is suggested to set the ``motor`` as the ``tachometer``'s ``target``.
+
+    Methods
+    -------
+    :py:meth:`apply`
+        Computes the ``pwm`` to apply to the ``motor`` in order to limit its absorbed electric current to be lower
+        or equal to ``limit_electric_current``, until the ``encoder``'s ``target`` rotating object's
+        ``angular_position`` equals the ``target_angular_position``.
+
+    Raises
+    ------
+    TypeError
+        - If ``encoder`` is not an instance of ``AbsoluteRotaryEncoder``,
+        - If ``tachometer`` is not an instance of ``Tachometer``,
+        - If ``motor`` is not an instance of ``DCMotor``,
+        - if ``target_angular_position`` is not an instance of ``AngularPosition``,
+        - if ``limit_electric_current`` is not an instance of ``Current``.
+    ValueError
+        - If the ``motor`` cannot compute ``electric_current`` property,
+        - if ``limit_electric_current`` is negative or null.
+    """
 
     def __init__(self,
                  encoder: AbsoluteRotaryEncoder,
@@ -155,7 +350,45 @@ class StartLimitCurrent(RuleBase):
         self.limit_electric_current = limit_electric_current
         self.target_angular_position = target_angular_position
 
-    def apply(self) -> Union[float, int]:
+    def apply(self) -> Union[None, float, int]:
+        r"""Computes the ``pwm`` to apply to the ``motor`` in order to limit its absorbed electric current to be lower
+        or equal to ``limit_electric_current``, until the ``encoder``'s ``target`` rotating object's
+        ``angular_position`` equals the ``target_angular_position``.
+
+        Returns
+        -------
+        float or int or None
+            PWM value to apply to the ``motor`` in order to limit its absorbed electric current to be lower or equal to
+            ``limit_electric_current``
+
+        Notes
+        -----
+        It checks the applicability condition, defined as:
+
+        .. math::
+            \theta \le \theta_t
+
+        where:
+
+        - :math:`\theta` is the ``encoder``'s ``target`` ``angular_position``,
+        - :math:`\theta_t` is the ``target_angular_position``.
+
+        If the applicability condition is not met, then it returns ``None``, otherwise it computes the ``pwm`` as:
+
+        .. math::
+            D \left( \dot{\theta} \right) = \frac{1}{2} \, \left( \frac{\dot{\theta}}{\dot{\theta}_0} +
+            \frac{i_{lim}}{i_{max}} + \sqrt{ \left( \frac{\dot{\theta}}{\dot{\theta}_0} \right)^2 +
+            \left( \frac{i_{lim}}{i_{max}} \right)^2 +
+            2 \frac{\dot{\theta}}{\dot{\theta}_0} \frac{i_{lim} - 2 i_0}{i_{max}} } \right)
+
+        where:
+
+        - :math:`\dot{\theta}` is the ``tachometer``'s ``target``'s angular speed,
+        - :math:`\dot{\theta}_0` is the ``motor`` no load angular speed (``no_load_speed``),
+        - :math:`i_{lim}` is the ``limit_electric_current`` parameter,
+        - :math:`i_{max}` is the ``motor`` maximum electric current (``maximum_electric_current``),
+        - :math:`i_0` is the ``motor`` no load electric current (``no_load_electric_current``).
+        """
         angular_position = self.encoder.get_value()
         angular_speed = self.tachometer.get_value()
         no_load_speed = self.motor.no_load_speed
@@ -171,7 +404,7 @@ class StartLimitCurrent(RuleBase):
                                                 maximum_electric_current)))
 
 
-def _compute_static_error(braking_angle: Angle, transmission: Transmission):
+def _compute_static_error(braking_angle: Angle, transmission: Transmission) -> Union[Angle, AngularPosition]:
     maximum_torque = transmission.chain[0].maximum_torque
     load_torque = transmission.chain[0].load_torque
 
@@ -188,7 +421,7 @@ def _compute_static_error(braking_angle: Angle, transmission: Transmission):
     return static_error
 
 
-def _compute_pwm_min(transmission: Transmission):
+def _compute_pwm_min(transmission: Transmission) -> Union[float, int]:
     maximum_torque = transmission.chain[0].maximum_torque
     if transmission.chain[0].time_variables['load torque']:
         load_torque = transmission.chain[0].time_variables['load torque'][0]
