08ab95b78cb880d4a7bf4db0c37f2a3105b17fb7
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-02-11 22:18:22 +0100

    DEV: create HelicalGear

--- gearpy/mechanical_objects/__init__.py
+++ gearpy/mechanical_objects/__init__.py
@@ -1,5 +1,6 @@
 from .dc_motor import DCMotor
 from .flywheel import Flywheel
+from .helical_gear import HelicalGear
 from .mating_roles import MatingMaster, MatingSlave
 from .mechanical_object_base import MechanicalObject, RotatingObject, MotorBase, GearBase, Role
 from .spur_gear import SpurGear
