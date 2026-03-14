8f6a591e4c451d232df11fea4d06cec180528c44
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2026-03-14 14:07:34 +0100

    DOC: add PositionAndVelocityControl docstrings

--- gearpy/motor_control/rules/position_and_velocity_control.py
+++ gearpy/motor_control/rules/position_and_velocity_control.py
@@ -7,6 +7,43 @@ from gearpy.units import TimeInterval, AngularSpeed
 
 
 class PositionAndVelocityControl(RuleBase):
+    """:py:class:`PositionAndVelocityControl <gearpy.motor_control.rules.position_and_velocity_control.PositionAndVelocityControl>`
+    object. \n
+    It can be used to force the angular position of a rotating object, tracked
+    by the ``encoder``, to follow an S curve trajectory from a starting to a
+    stopping position.
+
+    Methods
+    -------
+    :py:meth:`apply`
+        It computes the ``pwm`` to apply to the ``powertrain``'s motor in order
+        to stick to position and velocity set by the ``trajectory`` through the
+        two nested ``position_PID`` and ``velocity_PID``.
+
+    .. admonition:: Raises
+       :class: warning
+
+       ``TypeError``
+           - If ``encoder`` is not an instance of
+             :py:class:`AbsoluteRotaryEncoder <gearpy.sensors.absolute_rotary_encoder.AbsoluteRotaryEncoder>`,
+           - if ``tachometer`` is not an instance of
+             :py:class:`Tachometer <gearpy.sensors.tachometer.Tachometer>`,
+           - if ``powertrain`` is not an instance of
+             :py:class:`DCMotor <gearpy.powertrain.Powertrain>`,
+           - if ``position_PID`` is not an instance of
+             :py:class:`PIDController <gearpy.motor_control.utils.pid_controller.PIDController>`,
+           - if ``velocity_PID`` is not an instance of
+             :py:class:`PIDController <gearpy.motor_control.utils.pid_controller.PIDController>`,
+           - if ``trajecotry`` is not an instance of
+             :py:class:`SCurveTrajectory <gearpy.motor_control.utils.s_curve_trajectory.SCurveTrajectory>`.
+
+    .. admonition:: See Also
+       :class: seealso
+
+       :py:attr:`DCMotor.pwm <gearpy.mechanical_objects.dc_motor.DCMotor.pwm>` \n
+       :py:class:`PIDController <gearpy.motor_control.utils.pid_controller.PIDController>` \n
+       :py:class:`SCurveTrajectory <gearpy.motor_control.utils.s_curve_trajectory.SCurveTrajectory>`
+    """
 
     def __init__(
         self,
@@ -63,6 +100,43 @@ class PositionAndVelocityControl(RuleBase):
         self.__trajectory = trajecotry
 
     def apply(self) -> None | float | int:
+        r"""It computes the ``pwm`` to apply to the ``powertrain``'s motor in
+        order to stick to position and velocity set by the ``trajectory``
+        through the two nested ``position_PID`` and ``velocity_PID``.
+
+        Returns
+        -------
+        :py:class:`float` or :py:class:`int` or :py:obj:`None`
+            PWM value to apply to the ``powertrain``'s motor in order to stick
+            to the S curve trajectory.
+
+        .. admonition:: Notes
+           :class: tip
+
+           It computes a velocity reference through the ``position_PID`` as:
+
+           .. math::
+               \dot{\theta}_{ref} = PID_{pos}(\theta - \theta_{ref})
+
+           where:
+
+           - :math:`\dot{\theta}_{ref}` is the velocity reference,
+           - :math:`\theta` is the current angular position, tracked by the
+             ``encoder``,
+           - :math:`\theta_{ref}` is the reference angular position, get by the
+             S curve ``trajectory``.
+           
+           Then, it computes a PWM through the ``velocity_PID`` as:
+           
+           .. math::
+              PWM_{ref} = PID_{vel}(\dot{\theta} - \dot{\theta}_{ref})
+
+           where:
+
+           - :math:`PWM_{ref}` is the reference PWM,
+           - :math:`\dot{\theta}` is the current angular speed, tracked by the
+             ``tachometer``.
+        """
         current_angular_position = self.__encoder.get_value(unit="rad")
         set_angular_position = self.__trajectory.compute(
             self.__powertrain.time[-1].to("sec")
